cf61ff384eaf5b89564894a49fb7df25fdde93bc
Carl Vanderlip <carlv@codeaurora.org>
2011-04-28 13:53:07 -0700

    video:msm: Initial QSEED sharpening/smoothing API
    Adds an additional parameter (.dpp.sharp_strength) and reuses flag
    MDP_SHARPENING to mdp_overlay, to allow for overlay_set to request for
    sharpening/smoothing.
    
    The overlay parameter is designed to range between -127 and 127, with
    negative values infering smoothing, positive infering sharpening. This
    range of values is then mapped onto 7 levels for both smoothing and
    sharpening (i.e. ~ every increase/decrease of 16 aligns with an increased
    sharpening level).
    
    This commit only adds the API for sharpening, a patch that updates the
    QSEED table2 values is needed for the desired effect to be visible.
    
    Change-Id: Ib6522e43bde4f4a54e752258553822e78a95749d
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4.h

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -593,6 +593,15 @@ void mdp4_overlay_vg_setup(struct mdp4_overlay_pipe *pipe)
 			pipe->r_bit << 4 | pipe->b_bit << 2 | pipe->g_bit);
 	}
 
+	if (pipe->flags & MDP_SHARPENING) {
+		outpdw(vg_base + 0x8200,
+			mdp4_ss_table_value(pipe->req_data.dpp.sharp_strength,
+									0));
+		outpdw(vg_base + 0x8204,
+			mdp4_ss_table_value(pipe->req_data.dpp.sharp_strength,
+									1));
+	}
+
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
 
 	mdp4_stat.pipe[pipe->pipe_num]++;
@@ -2105,6 +2114,17 @@ int mdp4_overlay_set(struct fb_info *info, struct mdp_overlay *req)
 
 	pipe->flags = req->flags;
 
+	if (pipe->flags & MDP_SHARPENING) {
+		bool test = ((pipe->req_data.dpp.sharp_strength > 0) &&
+			((req->src_rect.w > req->dst_rect.w) &&
+			 (req->src_rect.h > req->dst_rect.h)));
+		if (test) {
+			pr_warn("%s: No sharpening while downscaling.\n",
+								__func__);
+			pipe->flags &= ~MDP_SHARPENING;
+		}
+	}
+
 	mdp4_stat.overlay_set[pipe->mixer_num]++;
 
 	if (ctrl->panel_mode & MDP4_PANEL_MDDI) {
@@ -2430,8 +2450,15 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req,
 
 	if (pipe->pipe_num >= OVERLAY_PIPE_VG1)
 		mdp4_overlay_vg_setup(pipe);	/* video/graphic pipe */
-	else
+	else {
+		if (pipe->flags & MDP_SHARPENING) {
+			pr_warn(
+			"%s: Sharpening/Smoothing not supported on RGB pipe\n",
+								     __func__);
+			pipe->flags &= ~MDP_SHARPENING;
+		}
 		mdp4_overlay_rgb_setup(pipe);	/* rgb pipe */
+	}
 
 	mdp4_mixer_blend_setup(pipe);
 	mdp4_mixer_stage_up(pipe);
