9cf3aa5c9d2280e03ddfc16820e8fc6735e79df2
Stepan Moskovchenko <stepanm@codeaurora.org>
2011-08-31 17:13:32 -0700

    drivers: iommu: Add flags to iommu_domain_alloc
    Add the ability to pass flags into the iommu_domain_alloc
    function to specify domain attributes.
    
    Change-Id: Ia33aa8d887a57f76d97ab8921385dcc551716846
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -35,7 +35,7 @@ struct iommu_domain {
 #define IOMMU_CAP_CACHE_COHERENCY	0x1
 
 struct iommu_ops {
-	int (*domain_init)(struct iommu_domain *domain);
+	int (*domain_init)(struct iommu_domain *domain, int flags);
 	void (*domain_destroy)(struct iommu_domain *domain);
 	int (*attach_dev)(struct iommu_domain *domain, struct device *dev);
 	void (*detach_dev)(struct iommu_domain *domain, struct device *dev);
@@ -53,7 +53,7 @@ struct iommu_ops {
 
 extern void register_iommu(struct iommu_ops *ops);
 extern bool iommu_found(void);
-extern struct iommu_domain *iommu_domain_alloc(void);
+extern struct iommu_domain *iommu_domain_alloc(int flags);
 extern void iommu_domain_free(struct iommu_domain *domain);
 extern int iommu_attach_device(struct iommu_domain *domain,
 			       struct device *dev);
@@ -79,7 +79,7 @@ static inline bool iommu_found(void)
 	return false;
 }
 
-static inline struct iommu_domain *iommu_domain_alloc(void)
+static inline struct iommu_domain *iommu_domain_alloc(int flags)
 {
 	return NULL;
 }
