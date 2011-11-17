34acd6e6b37ea8a2f94c1bb6ca9f5d2efc83348f
Jordan Crouse <jcrouse@codeaurora.org>
2011-11-17 13:39:21 -0700

    msm: kgsl: Add timestamp events
    Add support for triggering asynchronous events when a timestamp
    expires.  The infrastructure is generic enough to support different
    sorts of events and callbacks.  The first user of the event
    infrastructure is a way to release a genlock locks on behalf of a
    user space process.
    
    Change-Id: Ic0dedbadfe67d98a5678453cbe0ac6996ba5c68f
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -135,6 +135,14 @@ struct kgsl_mh {
 	int              mpu_range;
 };
 
+struct kgsl_event {
+	uint32_t timestamp;
+	void (*func)(struct kgsl_device *, void *, u32);
+	void *priv;
+	struct list_head list;
+};
+
+
 struct kgsl_device {
 	struct device *dev;
 	const char *name;
@@ -183,6 +191,7 @@ struct kgsl_device {
 	struct kobject pwrscale_kobj;
 	struct pm_qos_request_list *pm_qos_req_dma;
 	struct work_struct ts_expired_ws;
+	struct list_head events;
 };
 
 struct kgsl_context {
